7d846cd5fc2a94c1311cc24bcd30864b39c4a8a3
ethic41 <dahirmuhammad3@gmail.com>
2020-11-28 08:13:25 +0100

    added more contents

---
+++ learning/routed_app/routers/items.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+
+@router.get("/")
+async def read_items():
+    return [{"name": "Item Foo"}, {"name": "item Bar"}]
+
+
+@router.get("/{item_id}")
+async def read_item(item_id: str):
+    return {"name": "Fake Specific Item", "item_id": item_id}
+
+
+@router.put("/{item_id}", tags=["custom"], responses={403: {"description": "Opreation forbidden"}})
+async def update_item(item_id: str):
+    if item_id != "foo":
+        raise HTTPException(status_code=403, detail="You can only update the item: foo")
+    return {"itme_id": item_id, "name": "The Fighters"}
+
